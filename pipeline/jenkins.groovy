0d390a88a5bb038794a36fbd3cb6b24909da7655
Igor Kostyrenko <115296463+Igor-Kostyrenko@users.noreply.github.com>
2023-07-17 06:53:07 +0000

    add jenkins.groovy

---
+++ pipeline/jenkins.groovy
@@ -0,0 +1,104 @@
+pipeline {
+    agent any
+    parameters {
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
+        choice(name: 'TARGETARCH', choices: ['amd64', 'arm64'], description: 'Pick architecture')
+    }
+    environment{
+        REPO = 'https://github.com/Igor-Kostyrenko/kobot'
+        BRANCH = 'develop'
+        REGISTRY = 'xevis'
+        DOCKERHUB_CREDENTIALS=credentials('dockerhub')
+        MACOSHOST_CREDENTIALS=credentials('machost')
+    }
+    stages {
+        stage('clone') {
+            steps {
+                echo 'Clone repo'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+        stage('Login to DockerHUB') {
+
+            steps {
+                sh 'security unlock-keychain -p $MACOSHOST_CREDENTIALS_PSW'
+                sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+            }
+        }
+        stage('test') {
+            steps {
+                echo 'run test'
+                sh 'make test'
+            }
+        }
+        stage('build') {
+            parallel {
+                stage('Build Linux') {
+                    when { expression { params.OS == 'linux' || params.OS == 'all' } }
+                    steps {
+                        echo 'Building for Linux'
+                        sh 'make image TARGETOS=linux'
+                    }
+                }
+                stage('Build Darwin') {
+                    when { expression { params.OS == 'darwin' || params.OS == 'all' } }
+                    steps {
+                        echo 'Building for macos'
+                        sh 'make image TARGETOS=macos'
+                    }
+                }
+                stage('Build Windows') {
+                    when { expression { params.OS == 'windows'  || params.OS == 'all' } }
+                    steps {
+                        echo 'Building for Windows'
+                        sh 'make image TARGETOS=windows'
+                    }
+                }
+            }
+        }
+        stage('push') {
+            parallel {
+                stage('Push Linux to dockerhub') {
+                    when { expression { params.OS == 'linux' || params.OS == 'all' } }
+                    steps {
+                        sh 'make push TARGETOS=linux'
+                    }
+                }
+                stage('Push Darwin to dockerhub') {
+                    when { expression { params.OS == 'darwin' || params.OS == 'all' } }
+                    steps {
+                        sh 'make push TARGETOS=macos'
+                    }
+                }
+                stage('Push Windows to dockerhub') {
+                    when { expression { params.OS == 'windows' || params.OS == 'all' } }
+                    steps {
+                        sh 'make push TARGETOS=windows'
+                    }
+                }
+            }
+        }
+        stage('clean') {
+            parallel {
+                stage('Clean Linux image on MacOS host ') {
+                    when { expression { params.OS == 'linux' || params.OS == 'all' } }
+                    steps {
+                        sh 'make clean TARGETOS=linux'
+                    }
+                }
+                stage('Clean Darwin image on MacOS host') {
+                    when { expression { params.OS == 'darwin' || params.OS == 'all' } }
+                    steps {
+                        sh 'make clean TARGETOS=macos'
+                    }
+                }
+                stage('Clean Windows image on MacOS host') {
+                    when { expression { params.OS == 'windows' || params.OS == 'all' } }
+                    steps {
+                        sh 'make clean TARGETOS=windows'
+                    }
+                }
+            }
+        }
+    }
+}
